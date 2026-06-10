118eb35f9a3d29030fe2306dbab137b9d83eb19a
shreya yeole <ujwalayeole0408@gmail.com>
2026-06-10 15:47:14 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("Portfolio Website Loaded");
+const texts = [
+  "Frontend Developer",
+  "Web Developer",
+  "JavaScript Learner"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+
+const textElement = document.querySelector(".multiple-text");
+
+function typeText(){
+
+  if(charIndex < texts[textIndex].length){
+
+    textElement.textContent += texts[textIndex].charAt(charIndex);
+
+    charIndex++;
+
+    setTimeout(typeText, 100);
+
+  }else{
+
+    setTimeout(eraseText, 1000);
+  }
+}
+
+function eraseText(){
+
+  if(charIndex > 0){
+
+    textElement.textContent =
+      texts[textIndex].substring(0, charIndex - 1);
+
+    charIndex--;
+
+    setTimeout(eraseText, 50);
+
+  }else{
+
+    textIndex++;
+
+    if(textIndex >= texts.length){
+      textIndex = 0;
+    }
+
+    setTimeout(typeText, 500);
+  }
+}
+
+typeText();
